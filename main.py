a11f9a0a9da7273fe4b412c8e1e63eb911a29894
khaled.turkestani <kturk1419@gmail.com>
2024-07-14 14:30:47 +0300

    added image and columns

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 
 with st.container():
     with col1:
-        st.image("photo.png")
+        st.image("images/photo.png")
 
     with col2:
         st.title("Khaled Turkestani")
@@ -23,5 +23,20 @@ with st.container():
     st.text("<b>Below you can find some of the apps i have built using Python. Feel free to contact me anytime.</b>")
 
 with st.container():
+    df = pd.read_csv("data.csv", sep=";")
     with col3:
-        pd.read_csv("data.csv", sep=";")
+        for i, row in df[:10].iterrows():
+            st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+            st.write(f"[Cooming Soon]({row['url']})")
+
+    with empty_col:
+        st.empty()
+
+    with col4:
+        for i, row in df[10:].iterrows():
+            st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+            st.write(f"[Cooming Soon]{row['url']}")
