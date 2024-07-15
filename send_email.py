0a54eaa32477248248ee73ec6e7ed892e587e202
khaled.turkestani <kturk1419@gmail.com>
2024-07-15 15:57:00 +0300

    added method to sewnd email

--- send_email.py
+++ send_email.py
@@ -3,7 +3,19 @@ import smtplib, ssl
 host = 'smtp.gmail.com'
 port = 465
 
-email = 'kturk141@gmail.com'
-password = "xfiw yoyg sgbl phbw"
+username = 'kturk1419@gmail.com'
+password = 'nsxvivkxizgspwdh'
 
+receiver = 'kturk1419@gmail.com'
 context = ssl.create_default_context()
+
+message = """\
+Subject: Helloooooooooo
+How are you everyone ??????
+
+Goodbye
+"""
+
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
